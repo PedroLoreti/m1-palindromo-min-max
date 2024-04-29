aa6c9a4a8ed58752e4446bc038b277f57efe1906
Loreti <pedroloreti639@gmail.com>
2024-04-29 07:47:53 -0300

    Criada a Logica das funções

--- script.js
+++ script.js
@@ -1,7 +1,27 @@
 function isPalindrome(str){
-    /* Seu código aqui */
+    let inverso = "";
+    str = str.toLowerCase();
+    for (let i = str.length - 1; i >= 0; i--) {
+        inverso += str[i];
+    }
+
+    if (inverso.split(" ").join("") === str.split(" ").join("")) {
+        return true;
+    } else {
+        return false;
+    }
 }
 
 function arrayMaxMin(arr){
-    /* Seu código aqui */
+    let min = arr[0];
+    let max = arr[0];
+
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] > max) {
+            max = arr[i];
+        } else if (arr[i] < min) {
+            min = arr[i];
+        }
+    }
+    return [min, max];
 }
